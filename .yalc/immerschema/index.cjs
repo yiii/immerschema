ada62976fbbe40d459505dd6135a7cb6e51f0e7f
yiii <yiiizzz@gmail.com>
2025-06-25 20:17:29 +0300

    Remove deprecated 'immerschema' package and its related files. Clear dependencies in package.json and update yalc.lock signature. Clean up schemas and documentation by deleting unused files and references.

--- .yalc/immerschema/index.cjs
+++
@@ -1,1 +0,0 @@
-module.exports = require('./schemas/core.schema.json'); 
--- .yalc/immerschema/index.js
+++
@@ -1,3 +0,0 @@
-import core from './schemas/core.schema.json' assert { type: 'json' };
-export { core };
-export default core; 
